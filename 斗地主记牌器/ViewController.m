deeaf152f05cb123d7ca4ecd227c1840770d4c76
hugb <1154684853@qq.com>
2016-06-13 11:05:53 +0800

    Initial Commit

---
+++ 斗地主记牌器/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  斗地主记牌器
+//
+//  Created by huGb on 16/6/13.
+//  Copyright © 2016年 iyd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
